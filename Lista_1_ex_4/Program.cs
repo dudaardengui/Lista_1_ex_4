def1d0dc52eae5334434784c4a4c7837b71655cb
ALUNO <ALUNO@LABHPC06>
2024-02-27 15:02:18 -0300

    Adicionar arquivos de projeto.

---
+++ Lista_1_ex_4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista_1_ex_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A, B, X, RESULTADO;
+            Console.WriteLine("Digite um número:");
+            A = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um número:");
+            B = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um número:");
+            X = int.Parse(Console.ReadLine());
+
+            if (X < 5)
+            {
+                RESULTADO = (B - A) * X;
+                Console.WriteLine();
+                Console.WriteLine(RESULTADO);
+            }
+            else
+            {
+                RESULTADO = (A + B) * X;
+                Console.WriteLine();
+                Console.WriteLine(RESULTADO);
+            }
+            Console.ReadKey();
+        }
+    }
+}
